b1e6f8cf332cb89f81b5a7f21b23a64d726f942a
PeriodicChaos <44913068+PeriodicChaos@users.noreply.github.com>
2024-04-23 18:16:40 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def process_form():
+    Username = request.form.get("uname")
+    Password = request.form.get("psw")
+    print(Username, Password)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
